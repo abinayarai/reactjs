c39c200207a985fb78040f5050c61c9cbd91ae8d
anilrai21 <developers.avc@gmail.com>
2017-02-27 16:52:39 +0545

    First commit

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    entry: "./main.js",
+    output: {
+        path: __dirname,
+        filename: "index.js"
+    },
+    devServer: {
+      inline: true,
+      port: 9000
+   },
+    module: {
+      loaders: [
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader',
+
+          query: {
+             presets: ['es2015', 'react']
+          }
+        }
+      ]
+    }
+};
