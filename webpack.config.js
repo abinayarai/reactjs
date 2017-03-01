c134cb2dfbf1f3adbade653096ff6a0be884c82e
anilrai21 <developers.avc@gmail.com>
2017-03-01 18:09:09 +0545

    Added npm package in package.json

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,24 @@
 module.exports = {
-    entry: "./src/main.js",
-    output: {
-        path: __dirname + "/src/build",
-        filename: "index.js"
-    },
-    devServer: {
-      inline: true,
-      port: 9000
-   },
-    module: {
-      loaders: [
-        {
-          test: /\.jsx?$/,
-          exclude: /node_modules/,
-          loader: 'babel-loader',
+  entry: "./src/main.js",
+  output: {
+      path: __dirname + "/src/build",
+      filename: "index.js"
+  },
+  devServer: {
+    inline: true,
+    port: 9000
+ },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
 
-          query: {
-             presets: ['es2015', 'react']
-          }
+        query: {
+           presets: ['es2015', 'react']
         }
-      ]
-    }
+      }
+    ]
+  }
 };
